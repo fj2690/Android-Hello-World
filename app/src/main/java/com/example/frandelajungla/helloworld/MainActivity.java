55307b1a8cfdb2727aee9cf83e77e36645198dd5
Fran de la Jungla <fran.j2690@hotmail.com>
2018-08-27 14:47:39 +0200

    Segunda parte Hola Mundo, con pruebas creando File Resources para dispositivos según su anchura, y tocando un poco los estilos.

--- app/src/main/java/com/example/frandelajungla/helloworld/MainActivity.java
+++ app/src/main/java/com/example/frandelajungla/helloworld/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.frandelajungla.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greetingView = (TextView) findViewById(R.id.greeting);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return  true;//Indicamos a Android que hemos atendido el menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_bye) {
+            greetingView.setText(R.string.bye);
+        }
+        return true;
     }
 }
